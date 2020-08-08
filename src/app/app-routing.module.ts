1ac94a4490189c67edd95493cf827a89b7a9f76b
Thuso <48202895+thuso@users.noreply.github.com>
2020-08-08 08:00:40 +0200

    Created all components and html templates

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { BlogComponent } from './blog/blog.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoutes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch:'full' },
+    { path: 'home', component: HomeComponent},
+    { path: 'about', component: AboutMeComponent },
+    { path: 'skills', component: MySkillsComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'blog', component: BlogComponent },
+    { path: 'contacts', component: ContactsComponent },
+    { path: '**', component: NotFoundComponent}
+]
+@NgModule({
+    imports: [ 
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports:[ RouterModule]
+})
+export class AppRoutingModule{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutMeComponent,
+    MySkillsComponent,
+    ProjectsComponent,
+    BlogComponent,
+    ContactsComponent,
+    FooterComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
